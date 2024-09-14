a2669b2c5b6b759b2eb270e139b255bd14e5449d
Semi-koron <6jimoto6@gmail.com>
2024-09-14 14:49:04 +0900

    feat Top Page and feat button component

---
+++ src/components/pages/SignIn/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Link from "next/link";
+import style from "./index.module.css";
+import clsx from "clsx";
+import { Button } from "@/components/ui/Button";
+
+export const Signin: React.FC = () => {
+  return (
+    <>
+      <div className={clsx(style["title-wrapper"])}>
+        <h1>アカウント登録</h1>
+      </div>
+      <div className={clsx(style["window-style"])}>
+        <div className={clsx(style["input-wrapper"])}></div>
+        <Button>登録</Button>
+      </div>
+    </>
+  );
+};
---
+++ src/components/pages/Top/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Link from "next/link";
+import style from "./index.module.css";
+import clsx from "clsx";
+import { Button } from "@/components/ui/Button";
+
+export const Top: React.FC = () => {
+  return (
+    <>
+      <div className={clsx(style["title-wrapper"])}>
+        <h1>Omiai Eisa</h1>
+        <p>~ えいさーで見つかるピッタリな相手 ~</p>
+      </div>
+      <div className={clsx(style["button-wrapper"])}>
+        <Link href="/sign_up">
+          <Button buttonShape="square">Sign Up</Button>
+        </Link>
+        <Link href="/sign_in">
+          <Button buttonShape="square">Sign In</Button>
+        </Link>
+      </div>
+    </>
+  );
+};
